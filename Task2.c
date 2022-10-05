0f92ca17d95c5bdab792a0a21fef98db93242341
tanyaponomaryova <tat.ponom2004@gmail.com>
2022-10-05 20:37:02 +0300

    add abs() function, variables initialization, more informative output for user and variables' names, fix way to find partial quotient

--- Task2.c
+++ Task2.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
-int module(int x)
-{
-    if (x >= 0)
-        return x;
-    return -x; 
-}
+#include <stdlib.h>
 int main()
 {   
-    int a;
-    int b;
+    printf("This program finds partial quotient of the input numbers.\n");
+    int divident = 0;
+    int divisor = 0;
     int answer = 0;
-    printf("Enter a: ");
-    scanf("%d", &a);
-    printf("Enter b: ");
-    scanf("%d", &b);
-    int moduleOfa = module(a);
-    int moduleOfb = module(b);
-    if (b == 0)
-        printf("Result of division by zero is undefined.");
-    else if (a >= 0)
+    printf("Enter divident (integer): ");
+    int scanned = scanf("%d", &divident);
+    while (scanned == 0)
     {
-        while (a >= module(b))
-        {
-            a -= module(b);
-            answer++;
-        }
+        scanf("%*[^\n]");
+        printf("You didn't enter a number, try again: ");
+        scanned = scanf("%d", &divident);
     }
+    printf("Enter divisor (integer): ");
+    scanned = scanf("%d", &divisor);
+    while (scanned == 0)
+    {
+        scanf("%*[^\n]");
+        printf("You didn't enter a number, try again: ");
+        scanned = scanf("%d", &divisor);
+    }
+    int moduleOfDivident = abs(divident);
+    int moduleOfDivisor = abs(divisor);
+    if (divisor == 0)
+        printf("Result of division by zero is undefined.");
     else 
     {
-        while (moduleOfa > moduleOfb*answer)
+        while ((moduleOfDivident - answer * moduleOfDivisor >= moduleOfDivisor) || (divident < 0 && moduleOfDivident > answer * moduleOfDivisor))
+        {
             answer++;
+        }
+        if ((divident < 0) ^ (divisor < 0))
+        {
+            answer = -answer;
+        }
+        printf("Result of finding partial quotient of division: %d", answer);
     }
-    if (a * b >= 0)
-        printf("a / b = %d", answer);
-    else
-        printf("a / b = %d", -answer);
+    return 0;
 }
