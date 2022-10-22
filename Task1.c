2c027bbb5e5a48914aa876fe4e18885aa52a82c4
tanyaponomaryova <tat.ponom2004@gmail.com>
2022-10-22 15:46:29 +0300

    fix way of converting decimal numbers to binary, change way of printing binary numbers, add info about program for user

--- Task1.c
+++ Task1.c
@@ -3,11 +3,14 @@
 #include <stdbool.h>
 #include <locale.h>
 
-#define size (int)sizeof(int) * 8
+#define SIZE (int)sizeof(int) * 8 // размер типа int в битах
 
 void printBinaryNumber(int array[])
 {
-    for (int i = 0; i < size; i++)
+    int j = 0;
+    for (; j < SIZE - 1 && array[j] == 0; j++); // пропускаем незначащие нули и останавливаемся на первом разряде на случай если распечатываемое число == 0
+
+    for (int i = j; i < SIZE; i++)
     {
         printf("%d", array[i]);
     }
@@ -16,18 +19,18 @@ void printBinaryNumber(int array[])
 
 void convertToBinary(int decimalNumber, int binaryNumber[])
 {
-    int bit = 0b10000000;
-    for (int i = 0; i < size; i++)
+    int bit = 1;
+    for (int i = SIZE - 1; i >= 0; i--)
     {
         binaryNumber[i] = (decimalNumber & bit) ? 1 : 0;
-        bit = bit >> 1;
+        bit = bit << 1;
     }
 }
 
 void sumOfTwoBinaryNumbers(int firstNumber[], int secondNumber[], int sumOfNumbers[])
 {
     int carryToNextPosition = 0;
-    for (int i = size - 1; i >= 0; i--)
+    for (int i = SIZE - 1; i >= 0; i--)
     {
         sumOfNumbers[i] = (firstNumber[i] + secondNumber[i] + carryToNextPosition) % 2;
         carryToNextPosition = (firstNumber[i] + secondNumber[i] + carryToNextPosition) / 2;
@@ -38,7 +41,7 @@ int convertToDecimal(int binaryNumber[])
 {
     int decimalNumber = 0;
     int powerOfTwo = 1;
-    for (int i = size - 1; i >= 0; i--)
+    for (int i = SIZE - 1; i >= 0; i--)
     {
         decimalNumber += powerOfTwo * binaryNumber[i];
         powerOfTwo *= 2; 
@@ -48,37 +51,41 @@ int convertToDecimal(int binaryNumber[])
 
 int main()
 {
+    printf("This program takes two integers as input, converts them to binary numbers, \n"); 
+    printf("calculates their sum in binary representation and then converts their sum to decimal number!\n");
+
     printf("Enter first number: ");
     int firstNumber = 0;
     int scanned = scanf("%d", &firstNumber);
-    while (scanned == 0 || firstNumber <= 0)
+    while (scanned == 0)
     {
         scanf("%*[^\n]");
-        printf("You didn't enter a natural number, try again: ");
+        printf("You didn't enter a number, try again: ");
         scanned = scanf("%d", &firstNumber);
     }
+
     printf("Enter second number: ");
     int secondNumber = 0;
     scanned = scanf("%d", &secondNumber);
-    while (scanned == 0 || secondNumber <= 0)
+    while (scanned == 0)
     {
         scanf("%*[^\n]");
-        printf("You didn't enter a natural number, try again: ");
+        printf("You didn't enter a number, try again: ");
         scanned = scanf("%d", &secondNumber);
     }
 
     printf("Binary representation of first number: ");
-    int firstBin[size] = {0};
+    int firstBin[SIZE] = {0};
     convertToBinary(firstNumber, firstBin);
     printBinaryNumber(firstBin);
 
     printf("Binary representation of second number: ");
-    int secondBin[size] = {0};
+    int secondBin[SIZE] = {0};
     convertToBinary(secondNumber, secondBin);
     printBinaryNumber(secondBin);
 
     printf("Sum of entered numbers in binary representation: ");
-    int sumOfNumbers[size] = {0};
+    int sumOfNumbers[SIZE] = {0};
     sumOfTwoBinaryNumbers(firstBin, secondBin, sumOfNumbers);
     printBinaryNumber(sumOfNumbers);
 
