e08a297df411f8157cba603826f02e1f42d5c5de
tanyaponomaryova <tat.ponom2004@gmail.com>
2022-12-22 23:36:05 +0300

    remove print from isStringBalanced function, made function accept string as input, add test

--- Task2/Task2BracketsBalance.c
+++ Task2/Task2BracketsBalance.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include "../stack/stack.h"
 
+#define STRING_LENGTH 100
+
 typedef enum Error
 {
     MemoryAllocationError,
@@ -11,33 +13,28 @@ typedef enum Error
 
 bool isMatchingBrackets(char openingBracket, char closingBracket)
 {
-    if (openingBracket == '(' && closingBracket == ')')
-        return true;
-    if (openingBracket == '{' && closingBracket == '}')
-        return true;
-    if (openingBracket == '[' && closingBracket == ']')
-        return true;
-    return false;
+    return openingBracket == '(' && closingBracket == ')' ||
+           openingBracket == '{' && closingBracket == '}' ||
+           openingBracket == '[' && closingBracket == ']';
 }
 
-Error isStringBalanced()
+Error isStringBalanced(char *string)
 {
-    Stack* stackPtr = NULL;
+    Stack *stackPtr = NULL;
     createStack(&stackPtr);
     if (stackPtr == NULL)
     {
         return MemoryAllocationError;
     }
-    printf("Enter string to check brackets balance: ");
-    for (char scannedSymbol = getchar(); scannedSymbol != '\n'; scannedSymbol = getchar())
+    for (int i = 0; string[i] != '\0'; i++)
     {
-        switch (scannedSymbol)
+        switch (string[i])
         {
             case '(':
             case '{':
             case '[':
             {
-                int errorCode = push(stackPtr, scannedSymbol);
+                int errorCode = push(stackPtr, string[i]);
                 if (errorCode == -1)
                 {
                     freeStack(stackPtr);
@@ -54,9 +51,9 @@ Error isStringBalanced()
                     freeStack(stackPtr);
                     return Unbalanced;
                 }
-                int topOfStack;
+                int topOfStack = 0;
                 pop(stackPtr, &topOfStack);
-                if (!isMatchingBrackets((char)topOfStack, scannedSymbol))
+                if (!isMatchingBrackets((char) topOfStack, string[i]))
                 {
                     freeStack(stackPtr);
                     return Unbalanced;
@@ -74,18 +71,33 @@ Error isStringBalanced()
     return Unbalanced;
 }
 
+bool test()
+{
+    char *string1 = "{}((hello))[wo],({rld})!";
+    char *string2 = "{}((hello)[wo],(rld})!";
+    Error result1 = isStringBalanced(string1);
+    Error result2 = isStringBalanced(string2);
+    return result1 == Balanced && result2 == Unbalanced;
+}
+
 int main()
 {
-    Error result = isStringBalanced();
+    if (!test())
+    {
+        printf("Test is failed :(");
+        return -1;
+    }
+    printf("Enter string to check brackets balance: ");
+    char string[STRING_LENGTH] = {0};
+    scanf("%s", string);
+    Error result = isStringBalanced(string);
     if (result == Balanced)
     {
         printf("Entered string is balanced :)");
-    }
-    else if (result == Unbalanced)
+    } else if (result == Unbalanced)
     {
         printf("Entered string is unbalanced :(");
-    }
-    else
+    } else
     {
         printf("Memory allocation error :0");
     }
